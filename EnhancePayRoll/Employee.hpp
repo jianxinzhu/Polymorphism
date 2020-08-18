b282b309e473ec69ce4d3aadf9c67c0d932531fd
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-08-17 23:30:25 -0400

    Create Employee.hpp

---
+++ EnhancePayRoll/Employee.hpp
@@ -0,0 +1,30 @@
+/*
+name: jianxin zhu
+date: aug/17/2020
+*/
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+#include<string>
+
+class Employee
+{
+public:
+	Employee(const std::string&, const std::string&);
+	virtual ~Employee() = default;
+
+	void setName(const std::string&);
+	void setSSN(const std::string&);
+
+	std::string getName() const;
+	std::string getSSN() const;
+
+	virtual double earnning() const = 0;
+	virtual std::string toString() const;
+
+
+private:
+	std::string name;
+	std::string socialSecurityNumber;
+};
+#endif
