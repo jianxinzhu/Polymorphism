9660535eaad486f69e4b1f0ddc32611c37c6616d
jianxinzhu <39395027+jianxinzhu@users.noreply.github.com>
2020-08-17 23:31:11 -0400

    Create WeekSalary.hpp

---
+++ EnhancePayRoll/WeekSalary.hpp
@@ -0,0 +1,26 @@
+/*
+name: jianxin zhu
+date aug/17/2020
+*/
+
+#ifndef WEEKSALARY_H
+#define WEEKSALARY_H
+
+#include"Employee.h"
+
+class WeekSalary : public Employee
+{
+public:
+	WeekSalary(const std::string&, const std::string&, double = 0.0);
+	virtual ~WeekSalary() = default;
+
+	void setWeekSalary(double);
+	double getWeekSalary() const;
+
+	virtual double earnning() const override;
+	virtual std::string toString() const override;
+
+private:
+	double weekSalary;
+};
+#endif
